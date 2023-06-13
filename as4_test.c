687d721993d3b86b1087c2e2f2df60607f91a912
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2023-06-13 15:57:13 +0300

    MADE SEEK TESTS READABLE Moved seek tests to be method based Added option to run only seek or random tests based on -seek | -random flags

--- as4_test.c
+++ as4_test.c
@@ -14,16 +14,43 @@ int main(int argc, char** argv)
     // Reset flag = reset PRG to original seed 0x2A
     // print flag = print non-errors during sub-tests
     int print = 0;
+    int random = 0, seek = 0;
     for(int i = 0; i < argc; i++)
     {
         if (strcmp("-p", argv[i]) == 0)
-            print++;
+            print = 1;
+        if (strcmp("-seek", argv[i]) == 0)
+        {
+            seek = 1;
+        }
+        if (strcmp("-random", argv[i]) == 0)
+        {
+            random = 1;
+        }
     }
-    
+    if (seek == 1)
+    {
+        if (!random)
+        {
+            printf("SEEK TESTS:\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            test_seek(print);
+            return 0;
+        }
+    }
+    else
+    {
+        if (random)
+        {
+            printf("RANDOM TESTS:\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            test_random(print);
+            return 0;
+        }
+    }
+
     int success = 0;
     printf("SEEK TESTS:\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     success += test_seek(print);
-    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("\nPRNG TESTS:\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     success += test_random(print);
